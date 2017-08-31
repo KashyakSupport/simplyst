b550fcc9ebc489da063fb4fc8374a586d0a2dadb
crew@kashyak.com <crew@kashyak.com>
2017-09-01 00:22:47 +0530

    changes in login pages

--- main.go
+++ main.go
@@ -6,23 +6,30 @@ import (
 	"net/http"
 	"simplyst/config"
 
-	"gopkg.in/mgo.v2/bson"
-
 	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type session struct {
+	Sessionid string
+	UserName  string
+}
 
 type user struct {
 	UserName string
-	Password string
+	Password []byte
 	First    string
 	Last     string
 }
 
-var dbSessions = map[string]string{}
-
+//var dbSessions = map[string]string{}
 var tpl *template.Template
-
 var usernames = []user{}
+var sessionUsernames = []session{}
+
+//var sessions session
+var userflag bool
 
 func init() {
 
@@ -34,7 +41,10 @@ func main() {
 
 	http.HandleFunc("/", index)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/home", home)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
+
 	http.ListenAndServe(":8080", nil)
 
 }
@@ -44,18 +54,33 @@ func index(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", u)
 
 }
+
+func home(w http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/login", http.StatusSeeOther)
+
+		return
+	}
+	tpl.ExecuteTemplate(w, "home.html", nil)
+}
 func userFromForm(r *http.Request) (*user, error) {
 	user := &user{
 		UserName: r.FormValue("username"),
-		Password: r.FormValue("password"),
-		First:    r.FormValue("firstname"),
-		Last:     r.FormValue("lastname"),
+		//Password: r.FormValue("password"),
+		First: r.FormValue("firstname"),
+		Last:  r.FormValue("lastname"),
 	}
 	return user, nil
 
 }
 
 func signup(w http.ResponseWriter, req *http.Request) {
+	var objsession session
+	/*	if alreadyLoggedIn(req) {
+			http.Redirect(w, req, "/", http.StatusSeeOther)
+			return
+		}
+	*/
 	// process form submission
 	if req.Method == http.MethodPost {
 		user, err := userFromForm(req)
@@ -64,19 +89,11 @@ func signup(w http.ResponseWriter, req *http.Request) {
 			fmt.Println(err)
 		}
 
-		// get form values
-		/*	un := req.FormValue("username")
-			p := req.FormValue("password")
-			f := req.FormValue("firstname")
-			l := req.FormValue("lastname")
-		*/
-		/*
-			// username taken?
-			if _, ok := dbUsers[un]; ok {
-				http.Error(w, "Username already taken", http.StatusForbidden)
-				return
-			}
-		*/
+		bs, err := bcrypt.GenerateFromPassword([]byte(req.FormValue("password")), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
 		// username taken?
 		usernames, err := allUsers()
 		un := req.FormValue("username")
@@ -96,12 +113,14 @@ func signup(w http.ResponseWriter, req *http.Request) {
 			Name:  "session",
 			Value: sID.String(),
 		}
-		http.SetCookie(w, c)
+		//http.SetCookie(w, c)
 		//user.Sessionid = c.Value
+		user.Password = bs
 		err = config.Userscollection.Insert(user)
 
-		dbSessions[c.Value] = user.UserName
-		err = config.Sessionscollection.Insert(dbSessions)
+		objsession.UserName = user.UserName
+		objsession.Sessionid = c.Value
+		err = config.Sessionscollection.Insert(objsession)
 
 		/*dbSessions[c.Value] = un */
 
@@ -117,6 +136,64 @@ func signup(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "signup.html", nil)
 }
 
+func login(w http.ResponseWriter, req *http.Request) {
+
+	if alreadyLoggedIn(req) {
+		tpl.ExecuteTemplate(w, "/home.html", nil)
+		return
+	}
+
+	if req.Method == http.MethodPost {
+
+		// is there a username?
+		userflag = loginuser(req)
+
+		if userflag {
+			un := req.FormValue("username")
+			sessions, err := allUserNameFromSession()
+
+			if err != nil {
+				fmt.Println("error from login function", err)
+			}
+
+			for _, session := range sessions {
+
+				if session.UserName == un {
+
+					c := &http.Cookie{
+						Name:  "session",
+						Value: session.Sessionid,
+					}
+					fmt.Println("Cookie value before Set", c)
+					http.SetCookie(w, c)
+					fmt.Println("Cookie value after Set", c)
+					http.Redirect(w, req, "/home", http.StatusSeeOther)
+					//tpl.ExecuteTemplate(w, "home.html", nil)
+					return
+
+				}
+
+			}
+
+			// get seesion Id From Sssiond DB
+
+			/*	// create session
+				sID := uuid.NewV4()
+				c := &http.Cookie{
+					Name:  "session",
+					Value: sID.String(),
+				}
+
+				http.SetCookie(w, c)
+
+				dbSessions[c.Value] = un
+			*/
+		}
+
+	}
+	tpl.ExecuteTemplate(w, "login.html", nil)
+}
+
 func getUser(w http.ResponseWriter, req *http.Request) user {
 	// get cookie
 	c, err := req.Cookie("session")
@@ -129,13 +206,48 @@ func getUser(w http.ResponseWriter, req *http.Request) user {
 
 	}
 	http.SetCookie(w, c)
-
-	// if the user exists already, get user
 	var u user
-	/*if un, ok := dbSessions[c.Value]; ok {
-		u = dbUsers[un]
-	}*/
+	/*var s session
+	Sessionid := c.Value
+	//var dbSessions = map[string]string{}
+	//get session id
+	// get username based on session id
+	// get user details based on user name
+	fmt.Println(" c.value Sessionscollection error from get user function", c.Value)
+	//errr := config.Sessionscollection.Find(bson.M{}).All(&sessions)
+	errr := config.Sessionscollection.Find(bson.M{"Sessionid": Sessionid}).One(&s)
+	if errr != nil {
+		fmt.Println(" errr Sessionscollection error from get user function", errr)
+	}
+	//un := dbSessions[c.Value]
 
+		for _, dbsession := range sessions {
+			if dbsession.Sessionid == c.Value {
+				s = dbsession
+				//return s
+			}
+		}
+	errrr := config.Userscollection.Find(bson.M{"username": s.UserName}).One(&u)
+	if errrr != nil {
+		fmt.Println("errrr Userscollection error from get user function", errrr)
+	}
+	fmt.Println("getting session id from get user", un)
+	fmt.Println("getting session id from get user", c)
+	fmt.Println("getting session id from get user", c.Value)
+	usernames, err := allUsers()
+
+	if err != nil {
+		fmt.Println("error from login function", err)
+	}
+
+	for _, username := range usernames {
+		if username.UserName == un {
+			u = username
+			return u
+		}
+	}
+	//_, ok := dbUsers[un]
+	*/
 	return u
 }
 
@@ -150,4 +262,75 @@ func allUsers() ([]user, error) {
 	fmt.Println("values from allusers function", usernames)
 
 	return usernames, nil
+}
+func loginuser(req *http.Request) bool {
+	un := req.FormValue("username")
+	p := req.FormValue("password")
+
+	usernames, err := allUsers()
+
+	if err != nil {
+		fmt.Println("error from login function", err)
+	}
+
+	for _, username := range usernames {
+
+		if username.UserName == un {
+			userflag = true
+			err := bcrypt.CompareHashAndPassword(username.Password, []byte(p))
+			if err != nil {
+				//	http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+				fmt.Println("error from loginuser function", err)
+				return false
+			}
+
+			//return userflag
+		}
+
+	}
+	return userflag
+}
+
+func allUserNameFromSession() ([]session, error) {
+
+	err := config.Sessionscollection.Find(bson.M{}).All(&sessionUsernames)
+
+	if err != nil {
+		fmt.Println("error from allUserNameFromSession()", err)
+		return nil, err
+	}
+	fmt.Println("values from allUserNameFromSession", sessionUsernames)
+
+	return sessionUsernames, nil
+
+}
+
+func alreadyLoggedIn(req *http.Request) bool {
+
+	c, err := req.Cookie("session")
+	if err != nil {
+		return false
+	}
+
+	// get Session id
+	// Based Seesion Id Get UserName
+	// Check Whaeteher user Existed or not
+
+	sessions, err := allUserNameFromSession()
+
+	if err != nil {
+		fmt.Println("error from login function", err)
+	}
+
+	for _, session := range sessions {
+
+		if session.Sessionid == c.Value {
+
+			return true
+
+		}
+
+	}
+
+	return false
 }
